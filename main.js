296bc188c909bb1cd2201f4fcfe03db1a950e7d5
Lusa2006 <160507772+Lusa2006@users.noreply.github.com>
2024-03-25 11:49:43 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,4 +5,10 @@ for(let i=0;i <botoes.length;i++){
 
         for (let j=0; j<botoes
     }
-}
+botoes{j}.classlist.remove('ativo');
+textos{j}.classlist.remove('ativo');
+botoes{i}.classlist.add('ativo');
+textos{i}.classlist.add('ativo');
+const contadores=document.querySelectorAll('.contador');
+const tempoObjetivo1=new Date('2020-10-05t00:00:00');
+const tempoObjetivo3=new date |('2023')
